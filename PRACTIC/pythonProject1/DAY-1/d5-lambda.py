dcdebd3061ca8f6b9d0c61152c26bc98007b1620
ne_srv <ne_srv@mail.ru>
2024-11-11 14:11:54 +0300

    as

--- PRACTIC/pythonProject1/DAY-1/d5-lambda.py
+++ PRACTIC/pythonProject1/DAY-1/d5-lambda.py
@@ -3,6 +3,7 @@
 s = 'Hello Python from Russia'
 
 s = s.split()
+
 # как получить длины слов в коллекции?
 
 # 5, 6, 4, 6
@@ -21,3 +22,21 @@ print(max(s, key=len))
 print(sorted(s, key=len))
 
 a = [4, 3, -10, 1, 7, 12]
+
+a = sorted(a, key=lambda x: x % 2 == 0)
+
+print(a)
+
+
+lst = ['москва', "Москва",  "Тверь", "Смоленск", "Псков", "Рязань"]
+
+
+lst = sorted(lst, key=lambda x: (x[-1], x[-2], ord(x[0])))
+
+print(lst)
+
+
+
+
+
+
